3b85e5dbf350b4796aefa05f830e404743194561
Pedro Fraile <pedrofm@usal.es>
2022-10-25 08:19:00 +0200

    Creacion de la clase main y Trabajador

---
+++ horario_asprodes/src/data/Trabajador.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+/**
+ *
+ * @author pedro
+ */
+public class Trabajador {
+    String identification;
+    float horasTrabajadasAnio;
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public float getHorasTrabajadasAnio() {
+        return horasTrabajadasAnio;
+    }
+
+    public void setHorasTrabajadasAnio(float horasTrabajadasAnio) {
+        this.horasTrabajadasAnio = horasTrabajadasAnio;
+    }
+    
+    
+}
